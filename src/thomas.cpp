d768741e7cb9313f7a8cbb09812e26fb23874ab1
Thomas Kowalski <thom.kowa@gmail.com>
2020-03-16 17:49:23 +0100

    Project is now linked against LLVM

--- src/thomas.cpp
+++ src/thomas.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 #include <string>
 
+#include <llvm/ExecutionEngine/ExecutionEngine.h>
+
 extern "C" {
-    #include "sqliteInt.h"
-    #include "vdbeInt.h"
+#include "sqliteInt.h"
+#include "vdbeInt.h"
 }
 
 extern "C" {
-    int sqlite3VdbeExec(
-            Vdbe *p                    /* The VDBE */
-    ){
-        for(size_t i = 0; i < p->nOp; i++) {
-            std::cout << p->aOp[i].opcode << std::endl;
-        }
+int sqlite3VdbeExec(Vdbe *p) {
+    for (size_t i = 0; i < p->nOp; i++) {
+        std::cout << sqlite3OpcodeName(p->aOp[i].opcode) << std::endl;
     }
-
+    return SQLITE_OK;
+}
 }
