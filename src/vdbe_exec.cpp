51a1d3d2e5a5813d177d85930282fb5ff5181af3
Thomas Kowalski <thom.kowa@gmail.com>
2020-06-08 14:03:01 +0200

    Updated vdbe_exec_universal

--- src/vdbe_exec.cpp
+++ src/vdbe_exec.cpp
@@ -6,16 +6,32 @@ extern "C" {
 #ifndef ENABLE_JIT
 #error("Please define ENABLE_JIT")
 #endif
+#ifndef ENABLE_DEFAULT
+#error("Please define ENABLE_DEFAULT")
+#endif
+
+// Whether to use JIT for queries. Should be -1 if ENABLE_JIT && !ENABLE_DEFAULT or !ENABLE_JIT && ENABLE_DEFAULT
+char enableJit = -1;
 
 int jitVdbeStep(Vdbe *);
 
 int sqlite3VdbeExec(Vdbe *p) {
     auto tick = std::chrono::system_clock::now();
 
-#if ENABLE_JIT
+#if ENABLE_JIT && !ENABLE_DEFAULT
+    assert(enableJit == -1);
     auto step_return = jitVdbeStep(p);
 #else
+#if ENABLE_DEFAULT && !ENABLE_JIT
+    assert(enableJit == -1);
     auto step_return = sqlite3VdbeExec2(p);
+#else
+    int step_return;
+    if (enableJit)
+        step_return = jitVdbeStep(p);
+    else
+        step_return = sqlite3VdbeExec2(p);
+#endif
 #endif
 
     auto tock = std::chrono::system_clock::now();
