75c93d2fa8765f33bb0c3aa0b71fbd05196c59a9
Thomas Kowalski <thom.kowa@gmail.com>
2020-06-18 10:02:10 +0200

    Added stack_save.py, a simple tool for checking memory leaks in code

--- kowalski_util/stack_save.py
+++ kowalski_util/stack_save.py
@@ -1,1 +1,12 @@
 import os 
+
+path = "../src/mlir/lib/Standalone/Lowering"
+source_files = [path + "/" + x for x in os.listdir(path) if os.path.isfile(path + "/" + x)]
+
+print("Checking", source_files)
+for f in source_files:
+    with open(f) as fp:
+        content = fp.read()
+
+    if "saveStack" in content:
+        assert "restoreStack" in content, "Didn't find restoreStack in " + f
