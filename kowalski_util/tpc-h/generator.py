d1e3e568f93030a7208dea05d7b3a01aca40eb41
Thomas Kowalski <thom.kowa@gmail.com>
2020-06-29 15:38:38 +0200

    Reformatted generator.py

--- kowalski_util/tpc-h/generator.py
+++ kowalski_util/tpc-h/generator.py
@@ -5,10 +5,10 @@ import datetime
 import static_data as data
 from static_data import types as q_types
 
-
 args = sys.argv
 
 date_format = "%Y-%m-%d"
+
 
 def get_arg(name: str, default):
     args = [x.lower() for x in sys.argv[1:]]
@@ -20,13 +20,13 @@ def get_arg(name: str, default):
         args[i] = arg
 
     for i, arg in enumerate(args):
-        raw_arg = sys.argv[i]
         if arg == name:
             assert i < len(args) - 1
             return args[i + 1]
 
     return default
-            
+
+
 def output(sql: str):
     out_file_name = get_arg("o", None)
     if out_file_name is None:
@@ -35,9 +35,9 @@ def output(sql: str):
         with open(out_file_name, 'w') as f:
             f.write(sql)
 
+
 query_number = get_arg("query", None)
 query_count = int(get_arg("query-count", 1))
-
 
 queries = list()
 if query_number is not None:
@@ -51,6 +51,7 @@ def generate_int(info):
     max = info["max"]
     return random.randint(min, max)
 
+
 def generate_date(info):
     min = datetime.datetime.strptime(info["min"], date_format)
     max = datetime.datetime.strptime(info["max"], date_format)
@@ -58,6 +59,7 @@ def generate_date(info):
     random_days = random.randint(0, max_date_diff)
     new_date = min + datetime.timedelta(days=random_days)
     return new_date.strftime(date_format)
+
 
 def generate_float(info):
     if "value" in info:
@@ -67,14 +69,18 @@ def generate_float(info):
     max = info["max"]
     return min + (max - min) * random.random()
 
+
 def generate_region(info):
     return random.choice(data.regions)
+
 
 def generate_segment(info):
     return random.choice(data.segments)
 
+
 def generate_brand(info):
     return f"Brand#{random.randint(1, 5)}{random.randint(1, 5)}"
+
 
 def generate_type(info):
     if "syllables" in info:
@@ -83,10 +89,11 @@ def generate_type(info):
         n = 3
 
     return " ".join((
-        random.choice(data.types1),
-        random.choice(data.types2),
-        random.choice(data.types3)
-    )[:n])
+                        random.choice(data.types1),
+                        random.choice(data.types2),
+                        random.choice(data.types3)
+                    )[:n])
+
 
 def generate_container(info):
     return " ".join((
@@ -94,23 +101,29 @@ def generate_container(info):
         random.choice(data.containers2),
     ))
 
+
 def generate_nation(info):
     return random.choice(data.nations)
+
 
 def generate_p_name(info):
     words = [random.choice(data.p_name_words) for _ in range(5)]
     return " ".join(words)
 
+
 def generate_ship_mode(info):
     return random.choice(data.ship_modes)
+
 
 def generate_union(info):
     assert "items" in info
     return random.choice(info["items"])
 
+
 def generate_constant(info):
     assert "value" in info
     return info["value"]
+
 
 def generate_parameter(name, ty_info):
     type_name = ty_info["type"]
@@ -132,13 +145,14 @@ def generate_parameter(name, ty_info):
     generator = generators[type_name]
     return generator(ty_info)
 
+
 def generate_dict(cons):
     parameters = dict()
     for parameter_name, type_info in cons.items():
-        type_name = type_info["type"]
         parameters[parameter_name] = generate_parameter(parameter_name, type_info)
 
     return parameters
+
 
 result = ""
 for query_number in queries:
@@ -147,7 +161,6 @@ for query_number in queries:
 
     with open(f"{query_number}.sql") as f:
         sql = f.read()
-
 
     sql = sql.format(**parameter_dict)
     sql = sql.replace("\n", "    ")
