d6dd30471c38693728fee92c5f7227bd1d96c8c9
Thomas Kowalski <thom.kowa@gmail.com>
2020-07-24 16:44:21 +0200

    Added SSBM in benchmarking scripts

---
+++ kowalski_util/benchmarking/generator_ssb.py
@@ -0,0 +1,44 @@
+import os
+import sys
+import random
+import datetime
+from typing import Union
+
+args = sys.argv
+
+date_format = "%Y-%m-%d"
+
+
+from generator import output, get_arg
+
+def generate_query(number: Union[str, int]):
+    if isinstance(number, int):
+        number = str(number)
+    assert os.path.isfile(f"ssb/{number}.sql"), f"Couldn't find template for Query {number}"
+
+    with open(f"ssb/{number}.sql") as f:
+        sql = f.read()
+
+    return sql
+
+def main():
+    queries = [x.split('.')[0] for x in os.listdir('ssb')]
+    print(queries)
+    query_number = get_arg("query", None)
+    query_count = int(get_arg("query-count", 11))
+
+    if query_number is not None:
+        queries = [query_number] * query_count
+    else:
+        queries = [random.choice(queries) for _ in range(query_count)]
+
+    result = ""
+    for query_number in queries:
+        sql = generate_query(query_number)
+        result += sql
+        result += '\n'
+
+    output(result)
+
+if __name__ == '__main__':
+    main()
