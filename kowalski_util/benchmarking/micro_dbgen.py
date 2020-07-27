882e5cc01fe35744cff86367f93a4ab264cef98b
Thomas Kowalski <thom.kowa@gmail.com>
2020-07-27 17:01:28 +0200

    Enhanced micro_dbgen.py

--- kowalski_util/benchmarking/micro_dbgen.py
+++ kowalski_util/benchmarking/micro_dbgen.py
@@ -2,9 +2,38 @@ import sqlite3
 import random
 import datetime
 import sys
+import os
 
 
 class MicroGen:
+    @staticmethod
+    def random_name(length: int): 
+        alphabet = [chr(c) for c in range(ord('a'), ord('z') + 1)]
+        return "".join(random.choice(alphabet) for _ in range(length))
+
+    @staticmethod
+    def random_float(min: float, max: float):
+        return random.randrange(1000 * min, 1000 * max) / float(1000)
+    
+    @staticmethod
+    def random_date():
+        base = datetime.datetime(1970, 1, 1, 0, 0, 0, 0)
+        now = datetime.datetime.now()
+        diff = (now - base).total_seconds()
+        offset = random.randrange(0, int(diff))
+        result = base + datetime.timedelta(seconds=offset)
+        return result
+
+    @staticmethod
+    def random_int(min: int, max: int):
+        return random.randint(min, max)
+
+    @staticmethod
+    def get_permutation(n: int):
+        permut = list(range(n))
+        random.shuffle(permut)
+        return permut
+
     def __init__(self, scale_factor: float = 1.0):
         self.scale_factor: float = float(scale_factor)
 
@@ -49,7 +78,9 @@ class MicroGen:
         sql = "INSERT INTO Sales (S_ID, S_Price, S_Client, S_Date) VALUES (?, ?, ?, ?)"
 
         n_sales = int(10000 * self.scale_factor)
-        for i in range(n_sales):
+        ids = MicroGen.get_permutation(n_sales)
+
+        for i in ids:
             client = random.choice(self.clients)
             self.sales.append((
                 i,
@@ -60,32 +91,14 @@ class MicroGen:
 
         self.execute_many(sql, self.sales)   
 
-    @staticmethod
-    def random_name(length: int): 
-        alphabet = [chr(c) for c in range(ord('a'), ord('z') + 1)]
-        return "".join(random.choice(alphabet) for _ in range(length))
-
-    @staticmethod
-    def random_float(min: float, max: float):
-        return random.randrange(1000 * min, 1000 * max) / float(1000)
-    
-    @staticmethod
-    def random_date():
-        base = datetime.datetime(1970, 1, 1, 0, 0, 0, 0)
-        now = datetime.datetime.now()
-        diff = (now - base).total_seconds()
-        offset = random.randrange(0, int(diff))
-        result = base + datetime.timedelta(seconds=offset)
-        return result
-
-    @staticmethod
-    def random_int(min: int, max: int):
-        return random.randint(min, max)
-
     def insert_clients(self):
         print("Generating clients")
         sql = "INSERT INTO Clients VALUES (?, ?, ?, ?, ?)"
-        for i in range(int(self.scale_factor * 1000)):
+        
+        n_clients = int(self.scale_factor * 1000)
+        ids = MicroGen.get_permutation(n_clients)
+
+        for i in ids:
             self.clients.append((
                 i, 
                 MicroGen.random_name(16), 
@@ -99,8 +112,11 @@ class MicroGen:
     def insert_items(self):
         print("Generating items")
         sql = "INSERT INTO Items VALUES (?, ?, ?, ?)"
+        
         n_items = int(100000 * self.scale_factor)
-        for i in range(n_items):
+        ids = MicroGen.get_permutation(n_items)
+
+        for i in ids:
             self.items.append((
                 i,
                 MicroGen.random_name(64),
@@ -113,8 +129,10 @@ class MicroGen:
     def insert_sale_item(self):
         print("Generating SaleItems")
         sql = "INSERT INTO SaleItem VALUES (?, ?)"
+        
         n_sale_item = int(75000 * self.scale_factor)
-        for i in range(n_sale_item):
+        ids = MicroGen.get_permutation(n_sale_item)
+        for i in ids:
             item = random.choice(self.items)
             sale = random.choice(self.sales)
             self.sales_items.append((
@@ -126,6 +144,20 @@ class MicroGen:
 
 
 def main():
+    output = "micro.db"
+    if os.path.isfile(output):
+        overwrite = False
+        answer = input(f"File '{output}' exists. Overwrite? [y/N] ")
+        if answer == "y":
+            overwrite = True
+
+        if not overwrite:
+            sys.exit(2)
+        else:
+            os.unlink(output)
+                
+
+
     scale_factor = 1.0
     for i, x in enumerate(sys.argv):
         x = x.strip('-')
