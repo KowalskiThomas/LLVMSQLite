d203df16f8e8eacc126d43eebbe20dbf04c77b16
Thomas Kowalski <thom.kowa@gmail.com>
2020-09-01 10:12:22 +0200

    Updated aggregate.py

--- llvmsqlite_util/benchmarking/micro/13.sql
+++ llvmsqlite_util/benchmarking/micro/13.sql
@@ -1,6 +1,7 @@
 --- Aggregation
 -- This allows to check whether aggregating tuples
 -- is more / less efficient in the JIT.
-
-SELECT SUM(I_ID * I_Category + I_Price + (I_ID * I_Price) - (I_Category * (I_ID - I_Price * I_ID))) FROM Items;
-
+SELECT SUM(
+        I_ID * I_Category + I_Price + (I_ID * I_Price) - (I_Category * (I_ID - I_Price * I_ID))
+    )
+FROM Items;
--- llvmsqlite_util/benchmarking/micro/aggregate.py
+++ llvmsqlite_util/benchmarking/micro/aggregate.py
@@ -1,12 +1,16 @@
 import os
 
 sql_files = [x for x in os.listdir(".") if x.endswith("sql")]
+sql_files = list(sorted(sql_files, key = lambda x : int(x.split('.')[0])))
 
 result = ""
-for f in sql_files:
+for i, f in enumerate(sql_files):
+    i = i + 1
+    i = f.replace(".sql", "")
     with open(f) as sql:
-        result += sql.read()
-        result += "\n"
+        result += f"--- Query {i}\n"
+        result += sql.read().strip()
+        result += "\n\n\n"
 
 result = result.strip()
 with open("output.txt", 'w') as f:
